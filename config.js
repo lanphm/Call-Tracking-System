b66989470afc8930cb4e9576f126287760218ced
lanphm <lanphm@gmail.com>
2026-01-07 16:11:08 +0700

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // File này chỉ chứa thông tin kết nối, không chứa giao diện
 const CONFIG = {
     SUPABASE_URL: "https://ilxngmrnsghlgfqjxfni.supabase.co",
-    SUPABASE_ANON_KEY: "DÁN_MÃ_ANON_KEY_DÀI_CỦA_BẠN_VÀO_ĐÂY"
+    SUPABASE_ANON_KEY: "sb_publishable_-I0b_tYqzQF4Vk1i6nYGqA_pg0opuN_"
 };
