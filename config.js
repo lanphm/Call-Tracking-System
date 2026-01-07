ed205b6628d1fe010e6d1066ff126b04ae648939
lanphm <lanphm@gmail.com>
2026-01-07 16:10:11 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// File này chỉ chứa thông tin kết nối, không chứa giao diện
+const CONFIG = {
+    SUPABASE_URL: "https://ilxngmrnsghlgfqjxfni.supabase.co",
+    SUPABASE_ANON_KEY: "DÁN_MÃ_ANON_KEY_DÀI_CỦA_BẠN_VÀO_ĐÂY"
+};
